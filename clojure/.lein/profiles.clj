e73b788c0dcc91e1fb29eb798d72aa5e07b58899
Christian Westrom <c.westrom@westrom.xyz>
2021-07-18 01:23:54 -0400

    added jet

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,3 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.26.0"]
-                  [refactor-nrepl    "2.5.1"]
-                  [lein-ancient "0.7.0"]]}}
+{:user
+ {:dependencies [[borkdude/jet "0.0.6"]]
+  :aliases      {"jet" ["run" "-m" "jet.main"]}}
+ {:plugins [[cider/cider-nrepl "0.26.0"]
+            [refactor-nrepl    "2.5.1"]
+            [lein-ancient "0.7.0"]]}}
